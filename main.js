5729948bafbd09c1b66ce9221f89e1fac7f02501
denis091 <deniskovallenko@gmail.com>
2021-11-28 16:51:33 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,81 @@
+
+
+      function mouseOver() {
+        document.getElementById('button').style.display= 'block';
+      }
+      
+      function mouseOut() {
+        document.getElementById('button').style.display = 'none';
+      }
+
+      function mouseOver1() {
+        document.getElementById('button1').style.display= 'block';
+      }
+      
+      function mouseOut1() {
+        document.getElementById('button1').style.display = 'none';
+      }
+
+      function mouseOver2() {
+        document.getElementById('button2').style.display= 'block';
+      }
+      
+      function mouseOut2() {
+        document.getElementById('button2').style.display = 'none';
+      }
+
+      function mouseOver3() {
+        document.getElementById('button3').style.display= 'block';
+      }
+      
+      function mouseOut3() {
+        document.getElementById('button3').style.display = 'none';
+      }
+
+      function mouseOver4() {
+        document.getElementById('button4').style.display= 'block';
+      }
+      
+      function mouseOut4() {
+        document.getElementById('button4').style.display = 'none';
+      }
+
+      function mouseOver5() {
+        document.getElementById('button5').style.display= 'block';
+      }
+      
+      function mouseOut5() {
+        document.getElementById('button5').style.display = 'none';
+      }
+
+      function mouseOver6() {
+        document.getElementById('button6').style.display= 'block';
+      }
+      
+      function mouseOut6() {
+        document.getElementById('button6').style.display = 'none';
+      }
+
+      function mouseOver7() {
+        document.getElementById('button7').style.display= 'block';
+      }
+      
+      function mouseOut7() {
+        document.getElementById('button7').style.display = 'none';
+      }
+
+      function mouseOver8() {
+        document.getElementById('button8').style.display= 'block';
+      }
+      
+      function mouseOut8() {
+        document.getElementById('button8').style.display = 'none';
+      }
+
+      function mouseOver9() {
+        document.getElementById('button9').style.display= 'block';
+      }
+      
+      function mouseOut9() {
+        document.getElementById('button9').style.display = 'none';
+      }
